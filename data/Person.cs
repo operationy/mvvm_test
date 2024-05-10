f1cfebee45d50911d90f0ea7bcc0c2d92025ed90
Алексей Иванов <i.a.o-06.07.05@yandex.ru>
2024-05-10 11:15:12 +0300

    Добавьте файлы проекта.

---
+++ data/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm_test.data
+{
+    internal class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public static Person[] GetPerson()
+        {
+            var result = new Person[]
+            {
+                new Person(){LastName="Иванов", FirstName="Алексей"},
+                new Person(){LastName="Марго", FirstName="Робби"},
+                new Person(){LastName="Уйит", FirstName="Смит"}
+            };
+            return result;
+        }
+    }
+}
---
+++ viewmodel/PersonsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using mvvm_test.data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace mvvm_test.viewmodel
+{
+    internal class PersonsViewModel : DependencyObject
+    {
+
+
+        public string FilterText
+        {
+            get { return (string)GetValue(FilterTextProperty); }
+            set { SetValue(FilterTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterTextProperty =
+            DependencyProperty.Register("FilterText", typeof(string), typeof(PersonsViewModel), new PropertyMetadata(""));
+
+
+
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(PersonsViewModel), new PropertyMetadata(null));
+
+        public PersonsViewModel()
+        {
+            Items = CollectionViewSource.GetDefaultView(Person.GetPerson());
+        }
+    }
+}
